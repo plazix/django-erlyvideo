df0a814c4d693f38230a5549ce2363d596c44d52
Aleksandr Zorin <plazix@gmail.com>
2011-10-16 11:46:15 +0400

    добавлен setyp.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+from erlyvideo import get_version
+
+
+setup(
+    name='django-erlyvideo',
+    version=get_version().replace(' ', '-'),
+    description=u'Private messages',
+    license="BSD License",
+    author='Aleksandr Zorin (plazix)',
+    author_email='plazix@gmail.com',
+    url='https://github.com/plazix/django-erlyvideo',
+    download_url='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
