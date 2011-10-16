7088ad51c6fcd95cd1ea90bcad4f5dd1d62c9c7a
Aleksandr Zorin <plazix@gmail.com>
2011-10-16 23:57:13 +0400

    исправлены вьюшки для аутентификации при проигрывании и публикации потока

--- erlyvideo/conf/settings.py
+++ erlyvideo/conf/settings.py
@@ -3,9 +3,9 @@
 from django.conf import settings
 
 
-ERLYVIDEO_PUBLISH_AUTH_FUNC = getattr(settings, 'ERLYVIDEO_PUBLISH_AUTH_FUNC', None)
+ERLYVIDEO_PUBLISH_AUTH_FUNC = getattr(settings, 'ERLYVIDEO_PUBLISH_AUTH_FUNC', 'erlyvideo.views.public_auth_sample')
 
-ERLYVIDEO_PLAY_AUTH_FUNC = getattr(settings, 'ERLYVIDEO_PLAY_AUTH_FUNC', None)
+ERLYVIDEO_PLAY_AUTH_FUNC = getattr(settings, 'ERLYVIDEO_PLAY_AUTH_FUNC', 'erlyvideo.views.play_auth_sample')
 
 # с каких ip разрешен доступ к аутентификации и вызову обработчика события
 ERLYVIDEO_ACCESS_IPS = getattr(settings, 'ERLYVIDEO_ACCESS_IPS', None)
--- erlyvideo/views.py
+++ erlyvideo/views.py
@@ -3,13 +3,14 @@
 import logging
 
 from django.contrib.auth.models import User
+from django.core.urlresolvers import get_callable
 from django.http import HttpResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 
 from erlyvideo.conf.settings import ERLYVIDEO_PUBLISH_AUTH_FUNC, ERLYVIDEO_PLAY_AUTH_FUNC
 from erlyvideo.decorators import test_access
 from erlyvideo.models import ErlyVideoEvent
-from erlyvideo.signals import server_event, publish_auth, play_auth
+from erlyvideo.signals import server_event, publish_auth as _publish_auth, play_auth as _play_auth
 
 
 logger = logging.getLogger('erlyvideo')
@@ -42,9 +43,9 @@ def publish_auth(request):
     """
     Авторизация при побликации потока
     """
-    func = ERLYVIDEO_PUBLISH_AUTH_FUNC if ERLYVIDEO_PUBLISH_AUTH_FUNC else public_auth_sample
+    func = get_callable(ERLYVIDEO_PUBLISH_AUTH_FUNC) if ERLYVIDEO_PUBLISH_AUTH_FUNC else public_auth_sample
     if func(request.GET['ip'], request.GET['file'], request.GET['user_id'], request.GET['session_id']):
-        publish_auth.send(sender=User, ip=request.GET['ip'], file=request.GET['file'], user_id=request.GET['user_id'],
+        _publish_auth.send(sender=User, ip=request.GET['ip'], file=request.GET['file'], user_id=request.GET['user_id'],
             session_id=request.GET['session_id'])
         return HttpResponse()
     else:
@@ -57,9 +58,9 @@ def play_auth(request):
     """
     Авторизация при проигрывании потока
     """
-    func = ERLYVIDEO_PLAY_AUTH_FUNC if ERLYVIDEO_PLAY_AUTH_FUNC else play_auth_sample
+    func = get_callable(ERLYVIDEO_PLAY_AUTH_FUNC) if ERLYVIDEO_PLAY_AUTH_FUNC else play_auth_sample
     if func(request.GET['ip'], request.GET['file'], request.GET['user_id'], request.GET['session_id']):
-        play_auth.send(sender=User, ip=request.GET['ip'], file=request.GET['file'], user_id=request.GET['user_id'],
+        _play_auth.send(sender=User, ip=request.GET['ip'], file=request.GET['file'], user_id=request.GET['user_id'],
             session_id=request.GET['session_id'])
         return HttpResponse()
     else:
